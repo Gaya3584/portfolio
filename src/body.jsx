4ef4594da21e1db0a3c15c60726397b9e8f56023
Gaya3584 <gayugayus584@gmail.com>
2025-04-12 11:10:31 +0530

    updated

--- src/body.jsx
+++ src/body.jsx
@@ -1,6 +1,18 @@
+import './body.css'
+import meImg from './assets/me.png'
+
 function Body() {
-    return <div><h2>Body Section</h2></div>
-  }
-  
-  export default Body
-  
+  return (
+    <div className="body-section">
+      <div className="heading-container">
+        <h2 className="name-heading">GAYATHRI M A</h2>
+        <p className="summary">"I'm a passionate developer with experience in full-stack web development. I love solving problems and learning new technologies!"</p>
+      </div>
+      <div className="image-wrapper">
+        <img src={meImg} alt="Me" className="profile-img" />
+      </div>
+    </div>
+  )
+}
+
+export default Body
