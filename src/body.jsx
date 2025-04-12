5b378839119815741db2d70e49013d4584d82104
Gaya3584 <gayugayus584@gmail.com>
2025-04-12 20:46:35 +0530

    updated

--- src/body.jsx
+++ src/body.jsx
@@ -1,13 +1,21 @@
 import './body.css'
 import meImg from './assets/me.png'
+import 'animate.css';
+
 
 function Body() {
   return (
     <div className="body-section">
       <div className="heading-container">
+        <div class="container">
+        <div className="shape">
+          <div className="text">Hello!</div>
+        </div>
+        </div>
         <h2 className="name-heading">GAYATHRI M A</h2>
         <p className="summary">"I'm a passionate developer with experience in full-stack web development. I love solving problems and learning new technologies!"</p>
       </div>
+
       <div className="image-wrapper">
         <img src={meImg} alt="Me" className="profile-img" />
       </div>
--- src/mid.jsx
+++ src/mid.jsx
@@ -1,6 +1,18 @@
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { SiDevpost } from 'react-icons/si';
+import './mid.css';
+
 function Mid() {
-    return <div><h2>Mid Section</h2></div>
-  }
-  
-  export default Mid
-  
+  return (
+    <div className="mid-section">
+      <div className="social-icons">
+        <a href="https://github.com/Gaya3584" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/gayathri-m-a-12a823256/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href="https://www.instagram.com/__gayathri_m_a_?igsh=MTFyNGNyZnJzN240" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+        <a href="https://devpost.com/gayugayus584?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" target="_blank" rel="noopener noreferrer"><SiDevpost /></a>
+      </div>
+    </div>
+  );
+}
+
+export default Mid;
